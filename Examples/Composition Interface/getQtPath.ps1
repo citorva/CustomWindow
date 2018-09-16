d2f05f63f6b74adae18e37b9e098c23e2d6e7c2b
Venceslas <duet.venceslas@orange.fr>
2018-09-16 13:51:30 +0200

    [Add] Qt path detector

---
+++ Examples/Composition Interface/getQtPath.ps1
@@ -0,0 +1,30 @@
+﻿$letters = 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+
+$letters | ForEach-Object {
+    $qtpath = "$($_):\Qt"
+    if(Test-Path -Path $qtpath) {
+        $major = 5
+        $minor = 0
+        $rev = 0
+        $compiler = "msvc2017_64"
+        get-childitem $qtpath | ForEach-Object {
+             if ($_.Name -match "\d*\.\d*\.\d*") {
+                if (Test-Path -Path "$($qtpath)\$($_.Name)\$($compiler)\lib\cmake") {
+                    $pathdata = $_.Name.Split(".")
+                    if ($pathdata[0] -eq $major.ToString()) {
+                        if ($minor -lt [convert]::ToInt32($pathdata[1], 10)) {
+                            $minor = [convert]::ToInt32($pathdata[1], 10)
+                            $rev = [convert]::ToInt32($pathdata[2], 10)
+                        }
+                        elseif ($minor.ToString() -eq $pathdata[1]) {
+                            if ($rev -lt [convert]::ToInt32($pathdata[2], 10)) {
+                                $rev = [convert]::ToInt32($pathdata[2], 10)
+                            }
+                        }
+                    }
+                }
+             }
+        }
+        echo "$($qtpath)\$($major).$($minor).$($rev)\$($compiler)"
+    }
+}
